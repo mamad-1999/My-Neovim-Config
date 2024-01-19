7ec7a6c9b0922dc719d5fab529f73e86aa684c91
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2024-01-19 12:19:27 +0330

    terminal in neovim

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -91,6 +91,73 @@ local plugins = {
       }
       return opts
     end,
-  }
+  },
+  {
+    "akinsho/toggleterm.nvim", version = "*", config = true
+  },
+    {
+        "goolord/alpha-nvim",
+        lazy = true,
+    },
+    {
+        'akinsho/bufferline.nvim',
+        dependencies = {
+            'nvim-tree/nvim-web-devicons'
+        },
+    },
+    {
+        'folke/tokyonight.nvim',
+    },
+    {
+        "phaazon/hop.nvim",
+        lazy = true,
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = {
+            'nvim-tree/nvim-web-devicons'
+        },
+    },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        }
+    },
+    {
+        'nvim-tree/nvim-tree.lua',
+        dependencies = {
+            'nvim-tree/nvim-web-devicons',
+        },
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = {
+            {'nvim-lua/plenary.nvim'},
+        }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+    },
+    {
+        'folke/which-key.nvim',
+        lazy = true,
+    },
 }
 return plugins
